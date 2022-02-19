8a321928d8b455e5b566db3af639581c5099f9bb
kang-jin <dmssk13@gmail.com>
2022-02-19 23:44:02 +0900

    FEAT: navbar 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import MyNavbar from 'components/MyNavbar';
+
 function Home() {
   return (
-    <div className="App">
+    <div>
+      <MyNavbar />
       <header className="App-header">
         <h1>Hello Home!😀😘</h1>
       </header>
