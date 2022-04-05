91be883e9d9901fb41f2b7f91b1cc10d3fc318a6
kang-jiin <dmssk13@gmail.com>
2022-04-05 18:12:26 +0900

    FEAT(Project): update image import

---
+++ src/assets/img/images.ts
@@ -0,0 +1,17 @@
+import footballHome from 'assets/img/football/home.jpg';
+
+import kdkdHome from 'assets/img/kdkd/kdkd_home.png';
+
+import sgpgHome from 'assets/img/sgpg/4989.PNG';
+
+type typeImages = {
+  [key: string]: string;
+};
+
+const images: typeImages = {
+  footballHome,
+  kdkdHome,
+  sgpgHome
+};
+
+export default images;
