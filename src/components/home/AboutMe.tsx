32a1cb8862166887b5f6dcab94aab4f83e63d31b
kang-jin <dmssk13@gmail.com>
2022-04-11 00:09:17 +0900

    DESIGN: add css. update aboutme

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -1,34 +1,34 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import KSection from 'components/common/KSection';
 
 import dataset from 'assets/dataset/aboutme.json';
 
 const AboutMe = () => {
-  const textHeaderStyle: CSSProperties = {
-    color: '#000000'
-  };
-
-  const textBodyStyle: CSSProperties = {
-    color: '#6c757d'
-  };
-
   return (
     <KSection id="aboutme" title="저를 소개합니다" bgColor="light">
-      {dataset.map((row) => {
-        return (
-          <Row className="m-4 w-100">
-            {row.map((data) => {
-              return (
-                <Col sm={data.sm_size} md={data.md_size} lg={data.lg_size} className={data.className} key={data.id}>
-                  <h2 style={textHeaderStyle}>{data.title}</h2>
-                  <h5 style={textBodyStyle}>{data.contents}</h5>
+      {dataset.map((row) => (
+        <Row className="w-100" key={row.line}>
+          {row.all
+            ? row.detail.map((data) => (
+                <Col md="10" lg="8" className="p-4 mx-auto" key={data.id}>
+                  <h2 className="text-header">{data.title}</h2>
+                  <h5 className="text-contents">{data.contents}</h5>
                 </Col>
-              );
-            })}
-          </Row>
-        );
-      })}
+              ))
+            : row.detail.map((data, i) => (
+                <Col
+                  md="5"
+                  lg="4"
+                  className={i % 2 === 0 ? 'p-4 ms-auto' : 'p-4 me-auto'}
+                  key={data.id}
+                >
+                  <h2 className="text-header">{data.title}</h2>
+                  <h5 className="text-contents">{data.contents}</h5>
+                </Col>
+              ))}
+        </Row>
+      ))}
     </KSection>
   );
 };
--- src/components/home/Career.tsx
+++ src/components/home/Career.tsx
@@ -1,34 +1,26 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { Badge, Col, Row } from 'react-bootstrap';
 import KSection from 'components/common/KSection';
 
 import dataset from 'assets/dataset/career.json';
 
 const Career = () => {
-  const textHeaderStyle: CSSProperties = {
-    color: '#000000'
-  };
-
-  const textBodyStyle: CSSProperties = {
-    color: '#6c757d'
-  };
-
   return (
     <KSection id="career" title="Career" bgColor="light">
       {dataset.map((data) => {
         return (
           <Row className="m-4 align-items-center" key={data.year}>
-            <Col md="1" style={textHeaderStyle} className="ms-auto">
+            <Col md="1" className="text-header ms-auto">
               <h2>{data.year}</h2>
             </Col>
-            <Col md="7" style={textBodyStyle} className="my-2 me-auto">
+            <Col md="7" className="text-contents me-auto my-2">
               {data.contents.map((content, index) => {
                 return (
                   // eslint-disable-next-line react/no-array-index-key
                   <h5 className="my-4" key={index}>
                     {content.career} ({content.period})
                     {content.new === true && (
-                      <Badge color="success" pill className="ms-2">
+                      <Badge bg="info" pill className="ms-2">
                         NEW
                       </Badge>
                     )}
