4b41e47cfbea04c43ba78763e548c331c448d50c
kang-jin <dmssk13@gmail.com>
2022-04-10 22:32:25 +0900

    FIX(Skill): fixed ESLint error. update image import

--- src/assets/img/images.ts
+++ src/assets/img/images.ts
@@ -1,3 +1,20 @@
+import htmlLogo from 'assets/img/skills/html.png';
+import cssLogo from 'assets/img/skills/css.png';
+import javascriptLogo from 'assets/img/skills/900px-JavaScript-logo.png';
+import reactLogo from 'assets/img/skills/react.png';
+import nodejsLogo from 'assets/img/skills/icons8-nodejs-500.png';
+import oracleLogo from 'assets/img/skills/oracle.png';
+import mysqlLogo from 'assets/img/skills/mysql.png';
+import s3Logo from 'assets/img/skills/s3.png';
+import ec2Logo from 'assets/img/skills/ec2.png';
+import lambdaLogo from 'assets/img/skills/lambda.png';
+import sesLogo from 'assets/img/skills/ses.png';
+import dynamoDBLogo from 'assets/img/skills/dynamoDB.png';
+import vscodeLogo from 'assets/img/skills/vscode.png';
+import intellij from 'assets/img/skills/intellij.png';
+import gitLogo from 'assets/img/skills/git.png';
+import sourcetreeLogo from 'assets/img/skills/sourcetree.png';
+
 import footballHome from 'assets/img/football/home.jpg';
 
 import kdkdHome from 'assets/img/kdkd/kdkd_home.png';
@@ -9,6 +26,23 @@ type typeImages = {
 };
 
 const images: typeImages = {
+  htmlLogo,
+  cssLogo,
+  javascriptLogo,
+  reactLogo,
+  nodejsLogo,
+  oracleLogo,
+  mysqlLogo,
+  s3Logo,
+  ec2Logo,
+  lambdaLogo,
+  sesLogo,
+  dynamoDBLogo,
+  vscodeLogo,
+  intellij,
+  gitLogo,
+  sourcetreeLogo,
+
   footballHome,
   kdkdHome,
   sgpgHome
--- src/components/home/Skill.tsx
+++ src/components/home/Skill.tsx
@@ -3,23 +3,22 @@ import KSection from 'components/common/KSection';
 import { Card, Col, Row } from 'react-bootstrap';
 
 import dataset from 'assets/dataset/skill.json';
+import images from 'assets/img/images';
 
 const Skill = () => {
   return (
     <KSection id="skill" title="SKILL">
       <Row className="g-4">
-        {dataset.map((skill, sindex) => (
-          <Col xs="6" sm="6" md="3" key={sindex}>
+        {dataset.map((skill) => (
+          <Col xs="6" sm="6" md="3" key={skill.category}>
             <h3 className="h4 text-success font-weight-bold mt-4">
               {skill.category}
             </h3>
-            {skill.contents.map((item, i) => (
+            {skill.contents.map((item) => (
               <>
                 <Card.Title>{item.name}</Card.Title>
-                <Card style={{ width: '100px' }} key={i}>
-                  <Card.Img
-                    src={require('assets/img/' + item.img_path).default}
-                  />
+                <Card style={{ width: '100px' }} key={item.name}>
+                  <Card.Img src={images[item.img]} />
                 </Card>
               </>
             ))}
